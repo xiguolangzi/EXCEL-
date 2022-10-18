3ffc5034a400b0e33c88a5de8ff08ffedd5bf0f3
hubiao <hubiao19910129@gmail.com>
2022-10-18 14:58:45 +0200

    excel处理方法

---
+++ 处理excel/test.py
@@ -0,0 +1,37 @@
+# 1.导入xlwings
+import xlwings as xw
+'''
+# 2.创建app应用对象
+app1 = xw.App()
+pid = app1.pid
+print(pid)
+
+app2 = xw.App()
+pid = app2.pid
+print(pid)
+
+'''
+
+# 3.查看创建app的个数，即打开excel应用的个数
+apps = xw.apps
+print(apps.count)
+
+# 4.激活应用
+app3 = xw.apps[8444]
+app3.activate()
+
+# 5.添加工作簿
+wb = app3.books.add()
+
+# 6.设置表页
+sht = wb.sheets["sheet1"]
+
+# 7.操作表对应的单元格
+sht.range("A1").value = "hahaha"
+
+# 8.保存关闭 工作簿
+wb.save()
+wb.close()
+
+# 9.关闭应用
+app3.kill()
